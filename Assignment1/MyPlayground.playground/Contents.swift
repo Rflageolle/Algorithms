0aa25b89d5267cdbc2e514c09481457d10bce9bc
Ryan Flageolle <FlageMac@Ryans-iMac.local>
2019-09-23 21:22:01 -0600

    first assignment

---
+++ Assignment1/MyPlayground.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+// Quick sort algorithm
+func quickSort<T: Comparable>(array: inout[T], startIndex: Int, endIndex: Int) {
+    
+}
+
+func quickSort<T: Comparable>(array: inout[T]) {
+    quickSort(array, 0, array.count - 1)
+}
