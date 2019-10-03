cb7df06c519f372c8efba172aad35f82d4d023dc
RyanFlageolle <rflageol@msudenver.edu>
2019-10-03 11:38:05 -0600

    commented the code

--- Assignment2/word_trie.py
+++ Assignment2/word_trie.py
@@ -1,18 +1,20 @@
-'''
+"""
     Author: Ryan Flageolle
-    Assignment: To design a text prediction algorithm which prompts user input and
-        returns the next 10 possible words from the input given
+    Assignment: To design a text prediction algorithm which prompts user input
+        and returns the next 10 possible words from the input given
     Resources used: Learned about the Trie from:
         https://www.geeksforgeeks.org/trie-insert-and-search/
-'''
-
+"""
 import sys, select, tty, termios
 
 
 class NonBlockingConsole(object):
-    '''
-    from: https://stackoverflow.com/questions/16547486/read-raw-input-from-keyboard-in-python
-    '''
+    """
+        from: https://stackoverflow.com/questions/16547486/read-raw-input-from-
+        keyboard-in-python
+
+        use: to give me the ability to collect input and process it dynamically
+    """
     def __enter__(self):
         self.old_settings = termios.tcgetattr(sys.stdin)
         tty.setcbreak(sys.stdin.fileno())
@@ -30,6 +32,27 @@ class NonBlockingConsole(object):
         return False
 
 class Trie:
+    """Short summary.
+
+    Parameters
+    ----------
+    v : string
+        Description: value that is represented by that node.
+    isEndOfWord : boolean
+        Description: describes whether or not the node represents a complete
+                     word.
+
+    Attributes
+    ----------
+    children : dict
+        Description: A dictionary which is initialized as empty to hold more
+                     tries which represent if more letters follow it.
+    value : string
+        Description: Stores the parameter `v`.
+    isEndOfWord
+        Description: Stores the parameter `isEndOfWord`.
+
+    """
 
     def __init__(self, v='', isEndOfWord=False):
         self.children = dict()
@@ -37,6 +60,17 @@ class Trie:
         self.isEndOfWord = isEndOfWord
 
     def getNode(self, w):
+        """
+        Parameters
+        ----------
+        w : string
+            Description: A string to be searched for in the trie.
+
+        Returns
+        -------
+        Trie
+            Description: The Trie representing the last letter in `w`.
+        """
         curr = self
         for v in w:
             curr = curr.children.get(v, None)
@@ -45,10 +79,35 @@ class Trie:
         return curr
 
     def addNode(self, v, isword):
+        """
+        Parameters
+        ----------
+        v : string
+            Description: Value of the node to be added.
+        isword : boolean
+            Description: Value representing if the node is a full word.
+
+        Returns
+        -------
+        None
+            Description: Does not return, it adds a trie to `self`.
+
+        """
         self.children[v] = Trie(self.value + v, isword)
 
-
     def insert(self, key):
+        """
+        Parameters
+        ----------
+        key : String
+            Description: String to be reptesented in the trie.
+
+        Returns
+        -------
+        None
+            Description: Does not return, it adds a full word to `self`.
+
+        """
         curr = self
         l = len(key)
         for idx, c in enumerate(key):
@@ -59,8 +118,24 @@ class Trie:
                 next = curr.children[c]
             curr = next
 
-
     def starts_with(self, key, amount=None):
+        """
+        Parameters
+        ----------
+        key : string
+            Description: Value to be checked if the trie contains words
+                         which contain `key` as a prefix.
+        amount : int
+            Description: Defaults to none, else the ammount of words that the
+                         user would like to return.
+
+        Returns
+        -------
+        list[string]
+            Description: A list consiting of `amount` words which contain `key`
+                         as a prefix.
+
+        """
         start = self.getNode(key)
         if start is None:
             return []
@@ -75,14 +150,20 @@ class Trie:
 
         return words[:amount]
 
-
-
-'''
-    methods: main - is used to create the trie object as well as reading in the
-
-
-'''
 def create_trie(file):
+    """
+    Parameters
+    ----------
+    file : string
+        Description: URL of the text file representing the dictionary of words
+                     used to build the trie.
+
+    Returns
+    -------
+    Trie
+        Description: A Trie object representing the dictionary.
+
+    """
     t = Trie()
 
     x = 0
@@ -95,6 +176,9 @@ def create_trie(file):
     return t
 
 def __main__():
+    """
+    The driver method which builds the Trie and interacts with the user.
+    """
     print("Loading...")
     t = create_trie('words.txt')
     word = ''
@@ -135,6 +219,4 @@ def __main__():
                     sys.stdout.write('\b' * l + words)
                     sys.stdout.flush()
 
-
-#remove_dups()
 __main__()
