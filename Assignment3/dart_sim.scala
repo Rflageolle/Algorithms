6da19a42dcf8b153c6544cd5d4d89ae852a93ce4
RyanFlageolle <rflageol@msudenver.edu>
2019-10-23 12:59:02 -0600

    working on Assignment 3

---
+++ Assignment3/dart_sim.scala
@@ -0,0 +1,57 @@
+/*
+* Author: Ryan Flageolle
+* Assignment: Simulate the throwing of the darts and printout the approximate
+    value of Π computed for various iterations such as 1000, 10,000, 100,000,
+    1,000,000 and 100,000,000 (if time allows (it should)).
+*/
+
+import scala.util.Random
+import scala.math.{sqrt, exp}
+
+case class Dart(x: Double, y: Double)
+
+class DartSession(val n: Int) {
+
+  def throwDart(): Dart = {
+    val random = new Random();
+    val xbool = random.nextBoolean()
+    val x = if (xbool) (random.nextDouble() * (-1)) else (random.nextDouble() * (1))
+    val ybool = random.nextBoolean()
+    val y = if (ybool) (random.nextDouble() * (-1)) else (random.nextDouble() * (1))
+
+    new Dart(x, y)
+  }
+
+  // if distance from origin greater than 1 than not in circle
+  def isInCircle(d: Dart): Boolean = {
+    val dist = sqrt( (((d.x) - 0) * ((d.x) - 0)) + (((d.y) - 0) * ((d.y) - 0)) )
+    val is = if (dist < 1.0) true else false
+    is
+  }
+
+  // returns number of darts inside circle (M)
+  def session(): Int = {
+    var count = 0
+    for (a <- 0 to this.n) {
+      // println(s"----Throw #$a----")
+      var check = if (this.isInCircle(this.throwDart())) 1 else 0
+      // println(s"check = $check \n --------")
+      count = count + check
+    }
+    count
+  }
+
+  def results(m: Int): Double = (4.0 * m) / n
+
+  def simulate(): Unit = {
+    val m = this.session()
+    val r = this.results(m)
+    println(s"The approximation of 𝜋, throwing ${this.n} darts is: $r")
+  }
+}
+
+val sessions = List(1000, 10000, 100000, 1000000, 100000000)
+sessions.foreach(n => {
+  var s = new DartSession(n)
+  s.simulate()
+})
---
+++ Assignment3/prime_checker.scala
@@ -0,0 +1,71 @@
+/*
+  Author: Ryan Flageolle
+  Assignment: Write a program to generate a number randomly and test it against
+    k random values.
+*/
+import scala.util.Random
+import scala.util.control.Breaks._
+
+object Composite {
+  def isEven = (x: Int) => x % 2 == 0
+  def listDigits = (x: Int) => x.toString.map(_.asDigit).toList
+  def lastDigit = (l: List[Int]) => l(l.length - 1)
+  def lastDigitFive = (x: Int) => if (lastDigit(listDigits(x)) == 5) true else false
+  def sumDigitsDivByThree(x: Int): Boolean = {
+    val digits = listDigits(x)
+    var sum = 0
+    digits.foreach(x => sum += x)
+    sum % 3 == 0 && sum != 3
+  }
+}
+
+class PrimeChecker {
+  def isPrime(x: Int, l: List[Int]): Boolean = {
+    var isP = true
+    breakable {
+      l.foreach(n => {
+        if (x % n == 0){
+          isP = false
+          break
+        }
+      })
+    }
+    isP
+  }
+
+  def randomComposite(upto: Int): Int = {
+    var x = Random.nextInt(upto - 1) + 1
+    while (!Composite.isEven(x) && !Composite.lastDigitFive(x) && !Composite.sumDigitsDivByThree(x)) {
+      x = Random.nextInt(upto)
+    }
+    x
+  }
+
+  def randomList(k: Int, upto: Int): List[Int] = {
+    Seq.fill(k)({
+      Random.nextInt(upto - 1) + 1
+    }).toList
+  }
+
+  def randomComposites(k: Int, p: Int): List[Int] = {
+    Seq.fill(k)(randomComposite(p)).toList
+  }
+
+  def simulate(k: Int): Unit = {
+    var n = Random.nextInt(10000)
+    val pr = if (isPrime(n, randomList(k, n))) "" else " not"
+    println(s"The number: $n, is$pr prime.  Tested against $k numbers.")
+  }
+
+  def simVariableAccuracy(): Unit = {
+    val acc = List(10, 100, 1000, 10000)
+    val n = Random.nextInt(10000)
+    acc.foreach(k => {
+      val pr = if (isPrime(n, randomList(k, n))) "" else " not"
+      println(s"The number: $n, is$pr prime.  Tested against $k numbers.")
+    })
+  }
+}
+
+val pc = new PrimeChecker
+pc.simVariableAccuracy
