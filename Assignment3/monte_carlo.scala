5ca05c60b805ff1b7b8f102ba69c1cb652a0fb31
RyanFlageolle <rflageol@msudenver.edu>
2019-11-07 18:40:54 -0700

    dart sim for integration working

--- Assignment3/monte_carlo.scala
+++ Assignment3/monte_carlo.scala
@@ -5,57 +5,23 @@
   to use interesting functions for your tests.
 */
 import scala.util.Random
-import scala.math.{ pow, cos, sin }
+import scala.math.{ pow, cos, sin, Pi, abs }
 
-case class Dart(x: Int, y: Int)
-class Equation(val y: (Int) => Int) // describe as function of x
+def darts(f: (Double) => Double, start: Int, stop: Int, maxHeight: Int, trials: Int): Unit = {
+  var in = 0
+  for (_ <- 0 to trials) {
+    val x = (Random.nextDouble() * (stop - start)) + start
 
-class Box(val eqn: (Double) => Int, val width: Int, val height: Int) {
-  val halfw = width / 2
-  val halfh = height / 2
-  def xbounds(): Int = ( Random.nextInt(width - (width / 2)) - (width / 2) )
-  def ybounds(): Int = ( Random.nextInt(height - (height / 2)) - (height / 2) )
-}
+    val y = Random.nextDouble() * maxHeight
 
-
-class DartThrowing( val b: Box ) {
-
-  // val y = (x: Int) => 3 * ( (x) * (x) ) + 4 * x
-
-  def throwDart(): Dart = {
-    new Dart((this.b.xbounds), (this.b.ybounds))
+    if (y <= f(x)) { in = in + 1 }
   }
 
-  def below(): Boolean = {
-    val d = throwDart
-    val test = Dart(d.x, b.eqn(d.x))
-    d.y < test.y
-  }
-
-  def simulate(n: Int): Unit = {
-    var count = 0
-    for (_ <- 0 to n) {
-      if (below) count += 1
-    }
-    val percent = count.toDouble / n.toDouble
-    val integral = percent * ( (b.width) * (b.height) )
-    println(s"The percent of darts below the line was: $percent")
-    println(s"The integral from ${-1 * b.halfw} to ${(b.width - b.halfw)} is: $integral")
-  }
+  val guess = ((stop - start) * maxHeight) * (in / trials.toDouble)
+  println(s"The estimated integral of f(x) = ${guess}")
 
 }
 
-class MeanValues( val eqn: (Int) => Int, val width ) {
+val f = (x: Double) => abs(((cos(3 * Pi * x)) + (sin(x)))+5)
 
-  def randomDart(): Dart = {
-    def randomVal = ( Random.nextInt(width - (width / 2)) - (width / 2) )
-    new Dart( randomVal, eqn(randomVal))
-  }
-
-}
-
-val y = (x: Double) => (3 * (cos(x)) + (sin(x))).toInt
-val box = new Box(y, 20, 6)
-val darts = new DartThrowing(box)
-
-darts.simulate(100000)
+darts(f, 0, 10, 10, 100000)
